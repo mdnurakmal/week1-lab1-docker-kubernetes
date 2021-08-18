3cab505173a846cbc34320aee0efce9c3f99510e
mdnurakmal <mdnurakmal@gmail.com>
2021-08-18 21:53:01 +0800

    update

--- lamp.sh
+++ lamp.sh
@@ -3,6 +3,6 @@ docker stop lamp
 docker rm lamp
 cd LAMP
 docker build . -t mdnurakmal:lamp
-docker run -d -v $(pwd)/html:/var/www/localhost/htdocs/ -P --name lamp mdnurakmal:lamp
+docker run -d -t -v $(pwd)/html:/var/www/localhost/htdocs/ -P --name lamp mdnurakmal:lamp
 cd ..
 curl 172.17.0.2/index.php

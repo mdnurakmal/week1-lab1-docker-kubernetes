89b6e486b9815e6870924e61f2085b26a6cca175
mdnurakmal <mdnurakmal@gmail.com>
2021-08-19 00:24:57 +0800

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -4,7 +4,7 @@ docker rm lamp
 docker container prune -f
 cd LAMP
 docker build . -t mdnurakmal:lamp
-docker run -d -t -v $(pwd)/html:/var/www/localhost/htdocs/ -p 80:80/tcp --name lamp mdnurakmal:lamp
+docker run -d -t -v $(pwd)/html:/var/www/localhost/htdocs/ -p 80:80/tcp --name lamp -e MYSQL_ROOT_PASSWORD=password mdnurakmal:lamp
 cd ..
 sleep 15
 curl 172.17.0.2/index.php

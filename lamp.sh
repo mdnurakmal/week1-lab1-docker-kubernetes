4255d5a547142f6bcda481cd65cbee91690e7ea8
mdnurakmal <mdnurakmal@gmail.com>
2021-08-18 21:36:32 +0800

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker stop lamp
-
+docker rm lamp
+cd LAMP
 docker build . -t mdnurakmal:lamp
-docker run -d -v $(pwd)/html:/var/www/localhost/htdocs/ -P --name lamp mdnurakmal:lamp 
+docker run -d -v $(pwd)/html:/var/www/localhost/htdocs/ -P --name lamp mdnurakmal:lamp

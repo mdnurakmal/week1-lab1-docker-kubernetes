7d1e6e7aa5ff598077421f95f130984f730ee8cd
mdnurakmal <mdnurakmal@gmail.com>
2021-08-19 21:36:11 +0800

    Update run.sh

--- LEMP_Dockerfile/run.sh
+++ LEMP_Dockerfile/run.sh
@@ -16,7 +16,7 @@ wget http://files.directadmin.com/services/all/phpMyAdmin/phpMyAdmin-5.0.2-all-l
 tar zxvf phpMyAdmin-5.0.2-all-languages.tar.gz
 rm phpMyAdmin-5.0.2-all-languages.tar.gz
 mv phpMyAdmin-5.0.2-all-languages phpmyadmin
-
+chmod -R 777 /var/www/html
 
 echo phpmyadmin downloaded
 #keep container running

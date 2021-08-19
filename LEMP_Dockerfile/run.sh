58a22495a82deff72d81fbc7b9714f20d3428d7f
mdnurakmal <mdnurakmal@gmail.com>
2021-08-19 22:02:41 +0800

    update

--- LEMP_Dockerfile/run.sh
+++ LEMP_Dockerfile/run.sh
@@ -11,13 +11,6 @@ rc-service mariadb start
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'"
 printf "${MYSQL_ROOT_PASSWORD}\n n\n n\n n\n y\n y\n y\n" | mysql_secure_installation
 
-cd /var/www/html
-wget http://files.directadmin.com/services/all/phpMyAdmin/phpMyAdmin-5.0.2-all-languages.tar.gz
-tar zxvf phpMyAdmin-5.0.2-all-languages.tar.gz
-rm phpMyAdmin-5.0.2-all-languages.tar.gz
-mv phpMyAdmin-5.0.2-all-languages phpmyadmin
-chmod -R 755 /var/www/html/phpmyadmin
-
-echo phpmyadmin downloaded
+ln -s /usr/share/webapps/phpmyadmin/ /var/www/html/phpmyadmin
 #keep container running
 tail -f /dev/null

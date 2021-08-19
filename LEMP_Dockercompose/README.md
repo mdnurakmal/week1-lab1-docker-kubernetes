3daa0ef7977af51cad51f8b3e99cb788cac14f92
mdnurakmal <mdnurakmal@gmail.com>
2021-08-19 11:32:11 +0800

    update

---
+++ LEMP_Dockercompose/README.md
@@ -0,0 +1,10 @@
+# week1-lab1-docker-kubernetes
+
+Run docker-compose containing LAMP stack </br>
+Tag your image according to your dockerhub repository
+
+```shell
+docker-compose up -d
+```
+
+# References
---
+++ lemp_dockercompose.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# go to LAMP directory and run docker compose
+cd LEMP_Dockercompose
+
+# destroy existing containers and rebuild a new one
+docker-compose down
+docker-compose up -d
+
+cd ..
+
+# wait for services to load
+sleep 15
+curl localhost/index.php
